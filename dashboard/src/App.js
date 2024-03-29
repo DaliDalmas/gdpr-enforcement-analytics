6b9ddce68b780a003de316419c27ee9c41c7efed
Dalmas Otieno <dalmasotieno@Dalmass-MacBook-Air.local>
2024-03-29 15:54:39 +0300

    slider

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -4,6 +4,14 @@ import { useEffect, useState } from 'react';
 import * as d3 from 'd3'
 
 function App() {
+
+  const [maxFine, setMaxFine] = useState(1000000)
+  // Listen to the slider?
+  d3.select("#mySlider").on("change", function (d) {
+    const selectedValue = this.value
+    setMaxFine(selectedValue)
+  })
+
   const titleCase = strText => {
     var splitStr = strText.toLowerCase().split(' ');
     for (var i = 0; i < splitStr.length; i++) {
@@ -13,6 +21,8 @@ function App() {
   }
 
   const [data, setData] = useState(null)
+  const [df, setdf] = useState(null)
+
   useEffect(() => {
     const parseTime = d3.timeParse('%Y-%m-%d')
     const rowConverter = row => {
@@ -29,13 +39,46 @@ function App() {
       'http://127.0.0.1:8000/fines/?skip=0&limit=5000'
     ).then(dataset => {
       dataset = dataset.map(rowConverter)
-      dataset = dataset.sort((a,b)=> new Date(b.date) - new Date(a.date))
+      dataset = dataset.sort((a, b) => new Date(b.date) - new Date(a.date))
       setData(dataset)
+      setdf(dataset)
     })
+
+
   }, [])
+
+
+  useEffect(() => {
+    if (data){
+      setdf(data.filter(d => d.fine <= maxFine))
+    }
+  }, [maxFine,data])
+
+
   return (
     <div className="App">
-      {data? <Home data={data}/>: <></>}
+
+      <input type="range"
+              name="mySlider"
+              id="mySlider"
+              min={1000}
+              max={50000000} 
+              size={100}
+              style={{ width: '1000px' }} ></input>
+      <span>{maxFine}</span>
+      <div>
+        MAXIMUM FINE
+      </div>
+      <div>
+        AVERAGE FINE
+      </div>
+      <div>
+        MINIMUM FINE
+      </div>
+      <div>
+        MEDIAN FINE
+      </div>
+      {data ? <Home data={df} /> : <></>}
     </div>
   );
 }
